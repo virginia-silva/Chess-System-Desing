c4d50134a63c455256503a5696fb2454bb778269
Virginia Silva <virginia_hol@hotmail.com>
2022-01-12 16:50:59 -0300

    Cabeçalho do Programa

--- Chess System/src/application/Program.java
+++ Chess System/src/application/Program.java
@@ -3,6 +3,11 @@
  */
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import boardgame.Position;
+
 /**
  * @author vlucisil
  *
@@ -13,9 +18,24 @@ public class Program {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Testando o Terminal");
 		
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("                                  Criado por Virginia Silva !!!                                   ");
+		System.out.println();		
+		System.out.println("***************************************************************************************************");
+		System.out.println("______________________________________ JOGO DE XADREZ _____________________________________________");
+		System.out.println();
+		System.out.println("***************************************************************************************************");
+		System.out.printf("Nome Competidor 1 : ");
+		String c1 = sc.nextLine();
+		System.out.printf("Nome Competidor 2 : ");
+		String c2 = sc.nextLine();
+		System.out.println();		
+		System.out.println("Sejam bem vindos ao Jogo " + c1 + " e " + c2 + ", " + "sejam hosnestos e não trapassem !!!!");
+		
+		sc.close();
 	}
 
 }
