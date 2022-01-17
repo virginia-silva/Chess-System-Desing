3541540ea245eb85948c3dee85ddbba6a7a1f01f
Virginia Silva <virginia_hol@hotmail.com>
2022-01-17 19:10:42 -0300

    Colocando peças no tabuleiro

---
+++ Chess System/src/chess/pieces/King.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ * @author vlucisil
+ *
+ */
+public class King extends ChessPiece {
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return " K";
+	}
+}
